57d7cc086908f2fd551b322e03c6a418464fa47f
JavaYoshie Inc <40612283+Yoshie2000@users.noreply.github.com>
2018-12-08 13:52:09 +0100

    asd

---
+++ Assets/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Transform ball;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = ball.position + offset;
+        transform.rotation = Quaternion.Euler(new Vector3(0, ball.rotation.y, 0));
+    }
+
+}
